444918ad396fb06219cb3b5dc4f3f29e94b46b4e
Taban Kharrati <tabankharrati@gmail.com>
2024-10-20 16:38:10 +0330

    ADD migration

---
+++ migrate/migrate.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/cotopia-org/Event-Master/initializers"
+	"github.com/cotopia-org/Event-Master/models"
+)
+
+func init () {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToBD()
+}
+
+func main() {
+	initializers.DB.AutoMigrate(&models.Event{})
+}
