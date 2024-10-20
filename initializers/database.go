cdfd0cc0e51c9794f50c86e5e104cb33de43a0f4
Taban Kharrati <tabankharrati@gmail.com>
2024-10-20 16:37:32 +0330

    FIX BUG nil DB

--- initializers/database.go
+++ initializers/database.go
@@ -13,7 +13,7 @@ var DB *gorm.DB
 func ConnectToBD() {
 	var err error
 	dsn := os.Getenv("DB_URL")
-	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal("Failed to connect to DB\n", DB)
