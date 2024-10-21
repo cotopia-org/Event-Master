c05a0ada1bc4ff06b6370709d0fbe9aad8ff1047
Taban Kharrati <tabankharrati@gmail.com>
2024-10-21 13:10:38 +0330

    FIX logging err instead of DB!

--- initializers/database.go
+++ initializers/database.go
@@ -16,6 +16,6 @@ func ConnectToBD() {
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
-		log.Fatal("Failed to connect to DB\n", DB)
+		log.Fatal("Failed to connect to DB\n", err)
 	}
 }
