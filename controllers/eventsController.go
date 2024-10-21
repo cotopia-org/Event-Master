3aba5bf1a8768569b0b71c7072322b788d0c938f
Taban Kharrati <tabankharrati@gmail.com>
2024-10-21 13:31:44 +0330

    ADD create event router

---
+++ controllers/eventsController.go
@@ -0,0 +1,30 @@
+package controllers
+
+import (
+	"github.com/cotopia-org/Event-Master/initializers"
+	"github.com/cotopia-org/Event-Master/models"
+	"github.com/gin-gonic/gin"
+)
+
+func EventsCreate(c *gin.Context) {
+	// get data off req body
+	var body struct {
+		Title string
+		Body string
+	}
+	c.Bind(&body)
+
+	// create event
+	event := models.Event{Title: body.Title, Body: body.Body}
+	
+	result := initializers.DB.Create(&event)
+	if result.Error != nil {
+		c.Status(400)
+		return
+	}
+
+	// return it
+	c.JSON(200, gin.H{
+		"event": event,
+	})
+}
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/cotopia-org/Event-Master/controllers"
 	"github.com/cotopia-org/Event-Master/initializers"
 	"github.com/gin-gonic/gin"
 )
@@ -12,10 +13,8 @@ func init() {
 
 func main() {
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+
+	r.POST("/events", controllers.EventsCreate)
+
 	r.Run() // listen and serve on 0.0.0.0:PORT
 }
