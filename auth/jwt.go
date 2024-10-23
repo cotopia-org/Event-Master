4bacd51b415f92a55ad3aef59eb89a81f384fe71
Taban Kharrati <tabankharrati@gmail.com>
2024-10-23 14:48:08 +0330

    ADD JWT utility functions

---
+++ auth/jwt.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+var jwtSecret = []byte(os.Getenv("SECRET_KEY"))
+
+// Claims struct to contain the token payload
+type Claims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
+// GenerateJWT generates a JWT token
+func GenerateJWT(username string) (string, error) {
+	// Token expiration time
+	expirationTime := time.Now().Add(24 * time.Hour)
+
+	// Claims data
+	claims := &Claims{
+		Username: username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	// Create the token using your secret key
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtSecret)
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
+// ValidateToken validates a JWT token and returns the claims
+func ValidateToken(tokenString string) (*Claims, error) {
+	claims := &Claims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method")
+		}
+		return jwtSecret, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+	if !token.Valid {
+		return nil, fmt.Errorf("invalid token")
+	}
+	return claims, nil
+}
