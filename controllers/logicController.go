66f27648521deb41e8d558703bf78cc01de5d036
Taban Kharrati <tabankharrati@gmail.com>
2024-10-30 13:22:27 +0330

    ADD complement handler

--- controllers/logicController.go
+++ controllers/logicController.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/cotopia-org/Event-Master/logic"
 	"github.com/gin-gonic/gin"
@@ -27,16 +28,36 @@ func UnionSegments(c *gin.Context) {
 	c.JSON(http.StatusCreated, result)
 }
 
+func ComplementSegments(c *gin.Context) {
+	var segments []logic.LineSegment
+	minBoundFloat := 0.0
+	maxBoundFloat := 0.0
+	minBound := c.Param("minBound")
+	if s, err := strconv.ParseFloat(minBound, 64); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	} else {
+		minBoundFloat = s
+	}
 
-// func ComplementSegments(c *gin.Context) {
-// 	var segments []logic.LineSegment
-// 	minBound := c.Param("minBound")
-// 	maxBound := c.Param("maxBound")
-// 	if err := c.ShouldBindJSON(&segments); err != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 		return
-// 	}
-// 	result := logic.ComplementAll(segments, minBound, maxBound)
-// 	c.JSON(http.StatusCreated, result)
-// }
+	maxBound := c.Param("maxBound")
+	if s, err := strconv.ParseFloat(maxBound, 64); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	} else {
+		maxBoundFloat = s
+	}
 
+	if minBoundFloat > maxBoundFloat {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "minBound is greater than maxBound!"})
+		return
+	}
+
+	if err := c.ShouldBindJSON(&segments); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	result := logic.ComplementAll(segments, minBoundFloat, maxBoundFloat)
+	c.JSON(http.StatusCreated, result)
+}
